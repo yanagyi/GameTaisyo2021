f6b70b4498e4dacf3e99ad56f534359584498596
madssiggy <kanininaritainndahala@gmail.com>
2021-04-05 12:33:10 +0900

    [add]Test.scene追加。Resourceフォルダ、Scriptsフォルダ、スクリプト複数追加(Scripts/playerControl,CameraControl,UsableObject,Lift...要は前のプロトタイプで使ってたcsファイル)

---
+++ Assets/Scripts/playerControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControl :zenmaiObject
+{
+ //   public bool isZenmai;
+   public Rigidbody rb;
+   const float moveSpeed=0.25f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isZenmai == false)
+            return;
+            
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetAxis("Horizontal")<0) {
+            rb.MovePosition(gameObject.transform.position + new Vector3(0, 0, -moveSpeed));
+        }
+        if (Input.GetKey(KeyCode.RightArrow)||Input.GetAxis("Horizontal") > 0) {
+            rb.MovePosition(gameObject.transform.position + new Vector3(0, 0, moveSpeed));
+        }
+       
+    }
+
+}
---
+++ Assets/Scripts/zenmai.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zenmai : MonoBehaviour
+{
+    public string LTrigger;
+    public string RTrigger;
+    float rotateSpeed=1;
+    public GameObject oldParent;
+    public GameObject nowParent;
+    int state;
+    private enum statePattern
+    {
+        Idle=0,
+        Controlled,
+        ParentCheck,
+    }
+
+    public GameObject camera;
+    public cameraScript cameraScp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if ((nowParent = gameObject.transform.parent.gameObject) != null) {
+            nowParent.GetComponent<zenmaiObject>().isZenmai = true;
+            oldParent = nowParent;
+        } else {
+            nowParent = null;
+        }
+        
+        state = (int)statePattern.Idle;
+        camera = GameObject.Find("Main Camera");
+        cameraScp = camera.GetComponent<cameraScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (state) {
+            case (int)statePattern.Controlled:
+                Controlled();
+                break;
+            case (int)statePattern.ParentCheck:
+                ParentCheck();
+                break;
+            case (int)statePattern.Idle:
+            default:
+                Idle();
+                break;
+        }
+     //Debug.Log("now state:" + state + "@zenmai.cs.update");
+    }
+    //state==========================================================
+    private void Idle()
+    {
+        if (Input.GetKey(LTrigger)) {
+            nowParent.GetComponent<zenmaiObject>().isZenmai = false;
+            state = (int)statePattern.Controlled;
+            nowParent = null;
+            gameObject.transform.parent = null;
+            return;
+        }
+        if (nowParent == null) {
+            nowParent = oldParent;
+        }
+        cameraScp.ZenmaiOn();
+        transform.Rotate(new Vector3(0, rotateSpeed, 0));
+    }
+
+   private void Controlled() {
+        if (!Input.GetKey(LTrigger)) {
+            state = (int)statePattern.ParentCheck;
+            return;
+        }
+        //最寄りの円枚オブジェクト捜索
+        gameObject.transform.position+=new Vector3(0, Input.GetAxis("Vertical")/4, Input.GetAxis("Horizontal")/4);
+        cameraScp.ZenmaiOff();
+
+
+
+    }
+    private void ParentCheck() {
+
+        state = (int)statePattern.Idle;
+    }
+
+    public void Setparent(GameObject obj)
+    {
+        nowParent = obj;
+        nowParent.GetComponent<zenmaiObject>().isZenmai = true;
+        oldParent = nowParent;
+        gameObject.transform.parent = nowParent.transform;
+        gameObject.transform.localPosition = new Vector3(0, 1.3f, 0);
+
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (state != (int)statePattern.ParentCheck)
+            return;
+        //入力してんのに当たり判定なかったら戻る
+        
+        if (other.gameObject.tag == "zenmaiObj"|| other.gameObject.tag == "Player") {
+         //   Debug.Log("hit!at" + other.gameObject.name + "@zenmai.cs.OnCollisionEnter");
+            Setparent(other.gameObject);
+        } else {
+        //    Debug.Log("Don't hit any ZenmaiObj" + "@zenmai.cs.OnCollisionEnter");
+            Setparent(oldParent);
+        }
+    }
+    public int GetState() { return state; }//0がIdle,1がコントロール、2がチェック
+}
