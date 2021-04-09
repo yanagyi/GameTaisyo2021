e341c070295c107897ef42b469c033f231197246
yanagyi <yanagyi429@gmail.com>
2021-04-09 13:48:30 +0900

    柳澤用シーン追加、ゲームマネージャー見直し

--- Assets/Scripts/System/GameManager.cs
+++ Assets/Scripts/System/GameManager.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    //ゲームマネージャーのメモリ領域確保
     public static GameManager instance = null;
 
     private void Awake()
     {
+        //メモリ入れる
         if (instance == null)
         {
+            //メモリ
             instance = this;
             DontDestroyOnLoad(this.gameObject);
         }
+        //メモリが既に入っている場合破棄
         else
         {
             Destroy(this.gameObject);
