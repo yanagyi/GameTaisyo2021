7dc9131c66f287792c2ac0583cdd128475655ada
madssiggy <kanininaritainndahala@gmail.com>
2021-04-21 15:55:06 +0900

    ボタンスクリプト修正

--- Assets/Scripts/System/NoButton.cs
+++ Assets/Scripts/System/NoButton.cs
@@ -12,7 +12,7 @@ public class NoButton : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StageManagerScript = stageMng.GetComponent<RetryGame>();
+        StageManagerScript = stageMng.GetComponent<StageManager>();
     }
 
     // Update is called once per frame
--- Assets/Scripts/System/YesButton.cs
+++ Assets/Scripts/System/YesButton.cs
@@ -14,7 +14,7 @@ public class YesButton : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StageManagerScript = stageMng.GetComponent<AdvanceGame>();
+
     }
 
     // Update is called once per frame
